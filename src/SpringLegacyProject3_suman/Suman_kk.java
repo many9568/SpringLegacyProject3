3a78ab87388a9469fcd38bc1aadb57fc98b80546
Lee su man <many9568@gmail.com>
2023-12-12 16:21:40 +0900

    delete moon

--- src/SpringLegacyProject3_suman/Suman_kk.java
+++ src/SpringLegacyProject3_suman/Suman_kk.java
@@ -1,9 +1,8 @@
 package SpringLegacyProject3_suman;
 
 public class Suman_kk {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			System.out.println("hello world");
 	}
 
