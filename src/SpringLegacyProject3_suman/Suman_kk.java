4cf2ccbea1147c6a07d0282366d2ba6a22031775
Lee su man <many9568@gmail.com>
2023-12-12 16:16:18 +0900

    mylove

---
+++ src/SpringLegacyProject3_suman/Suman_kk.java
@@ -0,0 +1,10 @@
+package SpringLegacyProject3_suman;
+
+public class Suman_kk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello world");
+	}
+
+}
