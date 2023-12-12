d933fa41faa2caf8f3bfe811eeeb35136d88f8d6
many9568 <148834793+many9568@users.noreply.github.com>
2023-12-12 17:06:42 +0900

    Update Suman_kk.java

--- src/SpringLegacyProject3_suman/Suman_kk.java
+++ src/SpringLegacyProject3_suman/Suman_kk.java
@@ -3,7 +3,8 @@ package SpringLegacyProject3_suman;
 public class Suman_kk {
 	
 	public static void main(String[] args) {
-			System.out.println("hello world");
+			System.out.println("hello");
+		
 	}
 
 }
